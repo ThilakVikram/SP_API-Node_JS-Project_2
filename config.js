6dfb42b2c376186d47ff3f2e5a1681e6df2d73b0
Thilakvikram11 <thilakvikram13@gmail.com>
2026-03-20 18:14:00 +0530

    new change table name in config

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ export const DB_HOST = process.env.DB_HOST
 export const DB_PORT = process.env.DB_PORT
 
 export const WarehouseStockModelDetail = {
-    tableName:"WarehouseStock1"
+    tableName:"WarehouseStock"
 }
