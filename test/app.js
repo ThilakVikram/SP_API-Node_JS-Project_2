20fd20306c8f7a1a7113edc24a040eb39344939d
Thilakvikram11 <thilakvikram13@gmail.com>
2026-03-20 18:15:50 +0530

    new change table name in config 1

--- test/app.js
+++ test/app.js
@@ -1,4 +1,4 @@
-import { getCurrentUpdatedDateData } from "../src/controllers/warehousestock/getCurrentUpdatedDateData.1.js";
+import { getCurrentUpdatedDateData } from "../src/controllers/warehousestock/getCurrentUpdatedDateData.js";
 import { insertFbaInventoryToWarehouseStockModel } from "../src/services/sp_api/insertFbaInventoryToWarehouseStockModel.js";
 
 // insertFbaInventoryToWarehouseStockModel()
